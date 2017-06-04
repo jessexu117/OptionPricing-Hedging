04cdfeb216b23dd6436884f962ad5da6673dd728
JesseXu117 <jessexu117@outlook.com>
2017-06-05 00:19:42 +0800

    adjust the info of function

--- AsianGreeksLevy.m
+++ AsianGreeksLevy.m
@@ -1,8 +1,7 @@
 function [CallDelta,PutDelta,Gamma,CallTheta,PutTheta,Vega,CallRho,PutRho] ...
     = AsianGreeksLevy(AssetPrice,Strike,Sigma,Rates,Settle,ExerciseDates)
-% if nargin == 5
-%     Settle = today;
-% end
+%% function to caculate the Greeks of Asian options
+% in the file, a Levy model is used
 % Create RateSpec from the interest rate term structure
 StartDates = '12-March-2014';
 EndDates = '12-March-2020';
@@ -14,17 +13,14 @@ RateSpec = intenvset('ValuationDate', StartDates, 'StartDates', StartDates, ...
     'EndDates', EndDates, 'Rates', Rates, 'Compounding', ...
     Compounding, 'Basis', Basis);
 
-
 StockSpec = stockspec(Sigma, AssetPrice);
-                
-                
+                                
 OptSpec = 'call';                
 OutSpec = {'Delta';'Gamma';'Theta';'Vega';'Rho'};
 % Arithmetric Asian option using Levy model
 [CallDelta,Gamma,CallTheta,Vega,CallRho] = asiansensbylevy(RateSpec, StockSpec, OptSpec, Strike, Settle,...
                             ExerciseDates,'OutSpec',OutSpec);
-
-                        
+        
 % Geometric Asian option using Kemna-Vorst method
 % [DeltaLKV,s] = asiansensbykv(RateSpec, StockSpec, OptSpec, Strike, Settle,...
 %                         ExerciseDates,  'OutSpec', OutSpec)
